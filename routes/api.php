2fc6efc829abaff4b8e54f3243405daf66a2359f
unknown <dgloria.web@gmail.com>
2020-05-24 21:29:41 +0100

    main api working with get request

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@store');
+Route::delete('articles/{id}', 'ArticleController@destroy');
